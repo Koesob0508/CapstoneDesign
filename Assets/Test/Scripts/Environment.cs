f246f7a9228fde18753d869d3345ca6c64eaf2c8
Koesob3706 <epi3706@khu.ac.kr>
2022-05-04 17:40:18 +0900

    Reward Test

--- Assets/Test/Scripts/Environment.cs
+++ Assets/Test/Scripts/Environment.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Environment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float RewardSum;
+
+    private void Awake()
     {
-        
+        RewardSum = 0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        RewardSum -= 0.01f * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Goal"))
+        {
+            RewardSum += 1f;
+            Debug.Log("골인");
+        }
         
+        if(other.gameObject.CompareTag("Obstacle"))
+        {
+            RewardSum -= 1f;
+            Debug.Log("교통 사고");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("CenterLine"))
+        {
+            RewardSum -= 1f * Time.deltaTime;
+            Debug.Log("중앙선 침범");
+        }
     }
 }
